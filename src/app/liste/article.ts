1a164f034fa9a1c3b8e473032d3f433b6a3cdc3c
SullivanGDR <sullivan04gendre@gmail.com>
2024-10-25 14:46:07 +0200

    premier commit

---
+++ src/app/liste/article.ts
@@ -0,0 +1,11 @@
+export class Article {
+  name: string;
+  quantity: number;
+  price: number;
+
+  constructor(name: string, quantity: number, price: number) {
+    this.name = name;
+    this.quantity = quantity;
+    this.price = price;
+  }
+}
